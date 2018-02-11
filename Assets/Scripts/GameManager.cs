9889541e17133b6ffa0a63ae3e4ade9eb82ad708
diaosuyidsy <suyi.diao@vanderbilt.edu>
2018-02-11 15:09:49 -0600

    fine tuning a bit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	public GameObject Player;
+	public Transform initialLoaction;
+	// Use this for initialization
+	void Start ()
+	{
+		initialLoaction = Player.transform;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
